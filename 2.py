475954f4d7653ac20b96297b8fb19695b8267c77
kangelins2 <kirilova.angelina.kaktysik@gmail.com>
2023-12-17 20:44:40 +0300

    1

--- 2.py
+++ 2.py
@@ -1,7 +1,19 @@
 """
 Напишите скрипт, который прочитает файл 2.txt
-И создаст новый файл 2_raint.txt
+И создаст новый файл 2 raint.txt
 И точно так же запишет тот же текст, заменив все слова дождь на ДОЖДЬ
 (большими буквами)
-Учитывая окончания, например  дожде -> ДОЖДЕ
+Учитывая окончания, например дожде -> ДОЖДЕ
 """
+import re
+def main(filename):
+    f = open(filename, 'r', encoding="UTF-8")
+    text = f.read()
+    while True:
+        match = re.search('дожд(ь | е)', text)
+        if match is None:
+            break
+        start, end = match.span()
+        text = text[:start] + text[start: end].upper() + text[end:]
+    print(text)
+main("2.txt")
--- 3.py
+++ 3.py
@@ -36,3 +36,43 @@ def check(func, a, b):
 
 
 check(test, 5, 5)
+
+def divide_products(num_products, num_warehouses):
+  """
+  Разделяет продукцию максимально поровну на склады.
+
+  Args:
+    num_products: Количество продукции.
+    num_warehouses: Количество складов.
+
+  Returns:
+    Генератор, который возвращает количество продукции, отправленное на каждый склад.
+  """
+
+  if num_warehouses > num_products:
+    num_warehouses = num_products
+  remainder = num_products % num_warehouses
+  products_per_warehouse = num_products // num_warehouses
+  for i in range(num_warehouses):
+    if i < remainder:
+      yield products_per_warehouse + 1
+    else:
+      yield products_per_warehouse
+
+
+def test_divide_products():
+  """
+  Проверяет работу генератора.
+  """
+
+  assert list(divide_products(100, 5)) == [20, 20, 20, 20, 20]
+  assert list(divide_products(97, 5)) == [19, 19, 19, 20, 20]
+  assert list(divide_products(2, 3)) == [1, 1, 0]
+
+
+if __name__ == "__main__":
+  test_divide_products()
+
+products = divide_products(100, 5)
+for product in products:
+  print(product)
